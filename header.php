ed9779fb4df4ef2ff74633374463da35bc5234ae
MadMac MacAllister <madmac@PrimLinux.downwithdestruction.net>
2014-01-18 15:48:52 -0500

    Commented out Logo Block. Preparing for new style.

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <xen:hook name="header">
 <div id="header">
-	<xen:include template="logo_block" />
+        <xen:comment><xen:include template="logo_block" /></xen:comment>
 	<xen:include template="navigation" />
 	<xen:if is="{$canSearch}"><xen:include template="search_bar" /></xen:if>
 </div>
