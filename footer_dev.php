7286ce39cd3f94534d4e9d26ff133afea1e7124c
MadMac MacAllister <madmac@PrimLinux.downwithdestruction.net>
2014-01-20 08:50:09 -0500

    Added Footer Templates Updated Homepage Files

---
+++ footer_dev.php
@@ -0,0 +1,45 @@
+<div id="navigation" class="pageWidth {xen:if $canSearch, withSearch}">
+    <div class="pageContent">
+            <div class="footer">
+                <div class="inner">
+                    <div class='col-md-4'>
+                        <h4>Social</h4>
+                        <ul class="tile">
+                            <li><a href="#"><img src="theme/img/facebook.png" /></a></li>
+                            <li><a href="#"><img src="theme/img/twitter.png" /></a></li>
+                            <li><a href="#"><img src="theme/img/youtube.png" /></a></li>
+                            <li><a href="#"><img src="theme/img/google.png" /></a></li>
+                            <li><a href="#"><img src="theme/img/rss.png" /></a></li>
+                        </ul>                    
+                    </div>
+                    <div class='col-md-4'>
+                        <h4>Help & Support</h4>
+                        <ul>
+                            <li><a href="#">Wiki</a></li>
+                            <li><a href="#">Staff</a></li>
+                            <li><a href="#">Forum Issues</a></li>
+                            <li><a href="#">Game Server Issues</a></li>
+                            <li><a href="#">Report a member</a></li>
+                        </ul>                    
+                    </div>
+                    <div class='col-md-4'>
+                        <h4>Legal</h4>
+                        <ul>
+                            <li><a href="#">Terms & Conditions</a></li>
+                            <li><a href="#">Copyright</a></li>
+                            <li><a href="#">Credits</a></li>
+                        </ul>
+                    </div>
+                    <div class="clearfix"></div>
+                    <div class="footerfooter">
+                        &copy; Down With Destruction 2014, All Rights Reserved.
+                        <span class="pull-right">Site Created by DwD Dev Team</span>
+                    </div>
+                    <div class="printnotes">
+                        Forums powered by XenForo<br />
+                        Page time 0.0053 seconds<br />
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
