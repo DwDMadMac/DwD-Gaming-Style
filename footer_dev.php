adff50787754b3f5dda5bee4b2f887dd44979018
MadMac MacAllister <madmac@PrimLinux.downwithdestruction.net>
2014-01-21 07:50:42 -0500

    Fixed layout

--- footer_dev.php
+++ footer_dev.php
@@ -11,7 +11,9 @@
                             <li><a href="#"><img src="http://static.dwdg.net/theme/img/twitter.png" /></a></li>
                             <li><a href="#"><img src="http://static.dwdg.net/theme/img/youtube.png" /></a></li>
                             <li><a href="#"><img src="http://static.dwdg.net/theme/img/google.png" /></a></li>
-                            <li><a href="#"><img src="http://static.dwdg.net/theme/img/rss.png" /></a></li>
+                            <li><a href="{xen:link forums/-/index.rss}" rel="alternate" class="globalFeed" target="_blank" title="{xen:phrase rss_feed_for_x, 'title={$xenOptions.boardTitle}'}">
+                                    <img src="http://static.dwdg.net/theme/img/rss.png" /></a>
+                            </li>
                         </ul>                    
                     </div>
                     <div class='col-md-4'>
@@ -38,8 +40,20 @@
                         <span class="pull-right">Site Created by DwD Dev Team</span>
                     </div>
                     <div class="printnotes">
-                        Forums powered by XenForo<br />
-                        Page time 0.0053 seconds<br />
+                        <xen:hook name="footer">
+                            Forums powered by XenForo™ v1.2.2 ©2010-2013 XenForo Ltd.<br />
+                            <xen:if is="{$debugMode}">
+                                <xen:if hascontent="true">
+                                    <dl class="pairsInline debugInfo" title="{$controllerName}-&gt;{$controllerAction}{xen:if $viewName, ' ({$viewName})'}">
+                                        <xen:contentcheck>
+                                            <xen:if is="{$page_time}"><dt>{xen:phrase timing}:</dt> <dd><a href="{$debug_url}" rel="nofollow">{xen:phrase x_seconds, 'time={xen:number $page_time, 4}'}</a></dd></xen:if>
+                                            <xen:if is="{$memory_usage}"><dt>{xen:phrase memory}:</dt> <dd>{xen:phrase x_mb, 'size={xen:number {xen:calc "{$memory_usage} / 1024 / 1024"}, 3}'}</dd></xen:if>
+                                            <xen:if is="{$db_queries}"><dt>{xen:phrase db_queries}:</dt> <dd>{xen:number {$db_queries}}</dd></xen:if>
+                                        </xen:contentcheck>
+                                    </dl>
+                                </xen:if>
+                            </xen:if>
+                        </xen:hook>
                     </div>
                 </div>
             </div>
