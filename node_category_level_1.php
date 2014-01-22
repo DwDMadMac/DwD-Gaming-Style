7f14d28d6e99a2385827e56b7dafc67d5a2ab4f6
MadMac MacAllister <madmac@PrimLinux.downwithdestruction.net>
2014-01-22 01:09:56 -0500

    Removed all empty lines & Added Forum_list template

--- node_category_level_1.php
+++ node_category_level_1.php
@@ -3,14 +3,11 @@
     <li class="node category level_{$level} node_{$category.node_id} <xen:if is="in_array({$category.node_id}, array(94,95,96,97,98,99))">col-md-6</xen:if>" id="{xen:helper linktitle, $category.node_id, $category.title, 1}">
         <a href="{xen:link categories, $category}">
                     <div class="nodeInfo categoryNodeInfo categoryStrip">
-
                             <div class="categoryText">
                                     <h3 class="nodeTitle">{$category.title}</h3>
                                     <xen:if is="{$category.description}"><blockquote class="nodeDescription baseHtml">{xen:raw $category.description}</blockquote></xen:if>
                             </div>
-
                     </div>
-
             <xen:comment>
                     <xen:if is="{$renderedChildren}">		
                             <ol class="nodeList">
@@ -18,7 +15,6 @@
                             </ol>
                     </xen:if>
             </xen:comment>
-
                     <span class="tlc"></span>
                     <span class="trc"></span>
                     <span class="blc"></span>
