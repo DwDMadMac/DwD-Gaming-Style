74d0dd308a5c4e8b3e9c38a7bb20e645e69be327
MadMac MacAllister <madmac@PrimLinux.downwithdestruction.net>
2014-01-23 07:32:26 -0500

    Added wiki link & Added report a member link and amde it into a overlay popup for eas of access.

--- footer_dev.php
+++ footer_dev.php
@@ -17,11 +17,11 @@
                     <div class='col-md-4'>
                         <h4>Help & Support</h4>
                         <ul>
-                            <li><a href="#">Wiki</a></li>
-                            <li><a href="#">Staff</a></li>
+                            <li><a href="{xen:link wiki}">Wiki</a></li>
+                            <li><a href="http:dwdg.net/staff">Staff</a></li>
                             <li><a href="#">Forum Issues</a></li>
                             <li><a href="#">Game Server Issues</a></li>
-                            <li><a href="#">Report a member</a></li>
+                            <li><a href="{xen:link forms/file-a-report.5/respond}" class="OverlayTrigger">Report a member</a></li>
                         </ul>                    
                     </div>
                     <div class='col-md-4'>
