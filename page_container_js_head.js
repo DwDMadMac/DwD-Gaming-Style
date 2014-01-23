993bad10f0085dad75c044a0e30a1143b0b0f6ec
MadMac MacAllister <madmac@PrimLinux.downwithdestruction.net>
2014-01-23 09:17:43 -0500

    Removed JS script Link and added the script manually and removed tooltip which was causing somethings to disappear on hover.

--- page_container_js_head.js
+++ page_container_js_head.js
@@ -1,11 +1,70 @@
 	<script src="{$jQuerySource}"></script>	
 	<xen:if is="{$jQuerySource} != {$jQuerySourceLocal}">
 		<script>if (!window.jQuery) { document.write('<scr'+'ipt type="text/javascript" src="{$jQuerySourceLocal}"><\/scr'+'ipt>'); }</script>
-	</xen:if><xen:if is="{$xenOptions.uncompressedJs} == 1 OR {$xenOptions.uncompressedJs} == 3">
-	<script src="{$javaScriptSource}/jquery/jquery.xenforo.rollup.js?_v={$xenOptions.jsVersion}"></script></xen:if>	
+	</xen:if>
+        <xen:if is="{$xenOptions.uncompressedJs} == 1 OR {$xenOptions.uncompressedJs} == 3">
+            <script src="{$javaScriptSource}/jquery/jquery.xenforo.rollup.js?_v={$xenOptions.jsVersion}"></script>\n\
+        </xen:if>	
 	<script src="{xen:helper javaScriptUrl, '{$javaScriptSource}/xenforo/xenforo.js?_v={$xenOptions.jsVersion}'}"></script>
         <!-- Custom Navi JS -->
-        <script type="text/javascript" src="http://static.downwithdestruction.net/theme/js/custom.js"></script>
+        <script type="text/javascript">
+            function reInit() {
+                $("[title]");
+            }
+            $(function() {
+                reInit();
+
+                if ($('#nav').length) {
+                    var $el, leftPos, newWidth, newColour,
+                            $mainNav = $("#nav");
+                    $mainNav.append("<li id='nav-line'></li>");
+
+                    if (!$mainNav.find(".active:first").length) {
+                        $mainNav.find("li:first").addClass("active");
+                    }
+
+                    $("#nav>li").each(function() {
+
+                        if ((window.location.href.indexOf($(this).find("a:first").attr("href"))) > 0) {
+                            $mainNav.find("li").removeClass('active');
+                            $(this).addClass('active');
+                        }
+                    });
+
+                    var $magicLine = $("#nav-line");
+                    $magicLine
+                            .width($("#nav .active").width())
+                            .css("left", $("#nav .active").position().left)
+                            .data("origLeft", $("#nav .active").position().left)
+                            .data("origWidth", $("#nav .active").width())
+                            .data("bgColour", ($("#nav .active").attr("data-nav-colour")) ? $("#nav .active").data("nav-colour") : '78250e');
+                    $magicLine.css('background-color', "#" + $magicLine.data('bgColour'));
+
+
+                    $("#nav>li.navi>a").hover(function() {
+                        $el = $(this);
+                        leftPos = $el.offset().left - $mainNav.offset().left;
+                        newWidth = $el.parent().width();
+                        newColour = ($el.parent().attr("data-nav-colour")) ? $el.parent().data("nav-colour") : '78250e';
+                        $magicLine.stop().animate({
+                            left: leftPos,
+                            width: newWidth,
+                            backgroundColor: "#" + newColour
+                        }, 800, "easeOutBack");
+                    }, function() {
+                        $magicLine
+                                .data("origLeft", $("#nav .active").position().left)
+                                .data("origWidth", $("#nav .active").width())
+                                .data("bgColour", ($("#nav .active").attr("data-nav-colour")) ? $("#nav .active").data("nav-colour") : '78250e');
+                        $magicLine.stop().animate({
+                            left: $magicLine.data("origLeft"),
+                            width: $magicLine.data("origWidth"),
+                            backgroundColor: "#" + $magicLine.data("bgColour")
+                        }, 800, "easeOutBack");
+                    });
+                }
+            });
+        </script>
         <script type="text/javascript" src="http://static.downwithdestruction.net/theme/js/colour.jquery.min.js"></script>
         <xen:comment><script type="text/javascript" src="http://static.downwithdestruction.net/theme/js/bootstrap.min.js"></script></xen:comment>
         <!--XenForo_Require:JS-->
