d8ef5105046460f27e66ca02e8a05f22f98856b6
MadMac MacAllister <madmac@PrimLinux.downwithdestruction.net>
2014-01-25 03:09:58 -0500

    Relocated Node Stats under Node Last Post Box.

--- node_forum_level_2.php
+++ node_forum_level_2.php
@@ -23,28 +23,6 @@
                                     <blockquote class="nodeDescription {xen:if @nodeListDescriptionTooltips, nodeDescriptionTooltip} baseHtml" id="nodeDescription-{$forum.node_id}">{xen:raw $forum.description}</blockquote>
                             </xen:if>
                         </div>
-
-			<div class="nodeStats pairsInline">
-				<dl><dt>{xen:phrase discussions}:</dt> <dd>{xen:if $forum.privateInfo, '&ndash;', {xen:number $forum.discussion_count}}</dd></dl>
-				<dl><dt>{xen:phrase messages}:</dt> <dd>{xen:if $forum.privateInfo, '&ndash;', {xen:number $forum.message_count}}</dd></dl>
-				<xen:if is="{$renderedChildren} AND {$level} == 2 AND @nodeListSubForumPopup">
-					<div class="Popup subForumsPopup">
-						<a href="{xen:link forums, $forum}" rel="Menu" class="cloaked" data-closemenu="true"><span class="dt">{xen:phrase sub_forums}:</span> {xen:number $forum.childCount}</a>
-						
-						<div class="Menu JsOnly subForumsMenu">
-							<div class="primaryContent menuHeader">
-								<h3>{$forum.title}</h3>
-								<div class="muted">{xen:phrase sub_forums}</div>
-							</div>
-							<ol class="secondaryContent blockLinksList">
-							<xen:foreach loop="$renderedChildren" value="$child">
-								{xen:raw $child}
-							</xen:foreach>
-							</ol>
-						</div>
-					</div>
-				</xen:if>
-			</div>
 			
 			{xen:raw $nodeExtraHtml}
 		</div>
@@ -76,6 +54,27 @@
 		<div class="nodeControls">
 			<a href="{xen:link forums/index.rss, $forum}" class="tinyIcon feedIcon" title="{xen:phrase rss}">{xen:phrase rss}</a>
 		</div>
+			<div class="nodeStats pairsInline">
+				<dl><dt>{xen:phrase discussions}:</dt> <dd>{xen:if $forum.privateInfo, '&ndash;', {xen:number $forum.discussion_count}}</dd></dl>
+				<dl><dt>{xen:phrase messages}:</dt> <dd>{xen:if $forum.privateInfo, '&ndash;', {xen:number $forum.message_count}}</dd></dl>
+				<xen:if is="{$renderedChildren} AND {$level} == 2 AND @nodeListSubForumPopup">
+					<div class="Popup subForumsPopup">
+						<a href="{xen:link forums, $forum}" rel="Menu" class="cloaked" data-closemenu="true"><span class="dt">{xen:phrase sub_forums}:</span> {xen:number $forum.childCount}</a>
+						
+						<div class="Menu JsOnly subForumsMenu">
+							<div class="primaryContent menuHeader">
+								<h3>{$forum.title}</h3>
+								<div class="muted">{xen:phrase sub_forums}</div>
+							</div>
+							<ol class="secondaryContent blockLinksList">
+							<xen:foreach loop="$renderedChildren" value="$child">
+								{xen:raw $child}
+							</xen:foreach>
+							</ol>
+						</div>
+					</div>
+				</xen:if>
+			</div>
 		
 	</div>
 
