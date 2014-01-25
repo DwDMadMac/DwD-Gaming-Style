8c749b37b9dc373ec11634b8d72797391b86d78a
MadMac MacAllister <madmac@PrimLinux.downwithdestruction.net>
2014-01-25 03:19:45 -0500

    Moved subforums Stats to left of box.

--- node_forum_level_2.php
+++ node_forum_level_2.php
@@ -55,25 +55,25 @@
 			<a href="{xen:link forums/index.rss, $forum}" class="tinyIcon feedIcon" title="{xen:phrase rss}">{xen:phrase rss}</a>
 		</div>
 			<div class="nodeStats pairsInline">
-				<dl><dt>{xen:phrase discussions}:</dt> <dd>{xen:if $forum.privateInfo, '&ndash;', {xen:number $forum.discussion_count}}</dd></dl>
-				<dl><dt>{xen:phrase messages}:</dt> <dd>{xen:if $forum.privateInfo, '&ndash;', {xen:number $forum.message_count}}</dd></dl>
 				<xen:if is="{$renderedChildren} AND {$level} == 2 AND @nodeListSubForumPopup">
 					<div class="Popup subForumsPopup">
 						<a href="{xen:link forums, $forum}" rel="Menu" class="cloaked" data-closemenu="true"><span class="dt">{xen:phrase sub_forums}:</span> {xen:number $forum.childCount}</a>
 						
 						<div class="Menu JsOnly subForumsMenu">
-							<div class="primaryContent menuHeader">
-								<h3>{$forum.title}</h3>
-								<div class="muted">{xen:phrase sub_forums}</div>
-							</div>
-							<ol class="secondaryContent blockLinksList">
-							<xen:foreach loop="$renderedChildren" value="$child">
-								{xen:raw $child}
-							</xen:foreach>
-							</ol>
+                                                    <div class="primaryContent menuHeader">
+                                                        <h3>{$forum.title}</h3>
+                                                        <div class="muted">{xen:phrase sub_forums}</div>
+                                                    </div>
+                                                    <ol class="secondaryContent blockLinksList">
+                                                        <xen:foreach loop="$renderedChildren" value="$child">
+                                                            {xen:raw $child}
+                                                        </xen:foreach>
+                                                    </ol>
 						</div>
 					</div>
 				</xen:if>
+				<dl><dt>{xen:phrase discussions}:</dt> <dd>{xen:if $forum.privateInfo, '&ndash;', {xen:number $forum.discussion_count}}</dd></dl>
+				<dl><dt>{xen:phrase messages}:</dt> <dd>{xen:if $forum.privateInfo, '&ndash;', {xen:number $forum.message_count}}</dd></dl>
 			</div>
 		
 	</div>
