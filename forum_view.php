35d069a0b0000a47d776ee431272fb0a27791553
MadMac MacAllister <madmac@PrimLinux.downwithdestruction.net>
2014-01-27 01:20:11 -0500

    Some minor thread list styling.

--- forum_view.php
+++ forum_view.php
@@ -24,13 +24,6 @@
 <xen:container var="$bodyClasses">{xen:helper nodeClasses, $nodeBreadCrumbs, $forum}</xen:container>
 <xen:container var="$searchBar.forum"><xen:include template="search_bar_forum_only" /></xen:container>
 
-<xen:if is="{$canPostThread}">
-	<xen:set var="$newDiscussionButton"><a href="{xen:link 'forums/create-thread', $forum}" class="callToAction"><span>{xen:phrase post_new_thread}</span></a></xen:set>
-	<xen:if is="!{$renderedNodes}">
-		<xen:topctrl>{xen:raw $newDiscussionButton}</xen:topctrl>
-	</xen:if>
-</xen:if>
-
 <xen:if is="{$showPostedNotice}">
 	<div class="importantMessage">{xen:phrase message_submitted_displayed_pending_approval}</div>
 </xen:if>
@@ -50,9 +43,6 @@
 			<a href="{xen:link 'forums/watch', $forum}" class="OverlayTrigger" data-cacheOverlay="false">{xen:if $forum.forum_is_watched, '{xen:phrase unwatch_forum}', '{xen:phrase watch_forum}'}</a>
 		</xen:if>
 	</div>
-
-	<xen:pagenav link="forums" linkdata="{$forum}" linkparams="{$pageNavParams}" page="{$page}" perpage="{$threadsPerPage}" total="{$totalThreads}" />
-
 </div>
 
 <xen:hook name="forum_view_threads_before" params="{xen:array 'forum={$forum}'}" />
@@ -70,11 +60,16 @@
 	<div class="linkGroup"{xen:if '!{$ignoredNames}', ' style="display: none"'}><a href="javascript:" class="muted jsOnly DisplayIgnoredContent Tooltip" title="{xen:phrase show_hidden_content_by_x, "names={xen:helper implode, $ignoredNames, ', '}"}">{xen:phrase show_ignored_content}</a></div>
 	
 	<xen:pagenav link="forums" linkdata="{$forum}" linkparams="{$pageNavParams}" page="{$page}" perpage="{$threadsPerPage}" total="{$totalThreads}" />
+
 </div>
 <div class="discussionList section sectionMain" style="margin-bottom: 20px;">
 	<xen:include template="thread_list" />
 </div>
-	
+
+<div class="pageNavLinkGroup afterDiscussionListHandle">
+	<xen:pagenav link="forums" linkdata="{$forum}" linkparams="{$pageNavParams}" page="{$page}" perpage="{$threadsPerPage}" total="{$totalThreads}" />
+</div>
+
 <xen:if is="{$renderedNodes}">
 	<xen:include template="ad_forum_view_above_node_list" />
 	<xen:include template="node_list" />
