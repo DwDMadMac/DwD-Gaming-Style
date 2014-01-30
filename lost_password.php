fc65768ad132aa0e6957399f9cc3acb96a2b8b6d
MadMac MacAllister <madmac@PrimLinux.downwithdestruction.net>
2014-01-30 04:39:49 -0500

    Added new template lost password

---
+++ lost_password.php
@@ -0,0 +1,25 @@
+<xen:title>{xen:phrase lost_password}</xen:title>
+
+<form action="{xen:link 'lost-password/lost'}" method="post" class="xenForm formOverlay">
+	{xen:phrase if_forgotten_your_password_use_form_to_reset}
+	
+	<dl class="ctrlUnit">
+		<dt><label for="ctrl_username_email">{xen:phrase name_or_email}:</label></dt>
+		<dd><input type="text" name="username_email" class="textCtrl" id="ctrl_username_email" autofocus="true" /></dd>
+	</dl>
+
+	<xen:if hascontent="true">
+		<fieldset>
+			<xen:contentcheck>
+				<xen:include template="helper_captcha_unit" />
+			</xen:contentcheck>
+		</fieldset>
+	</xen:if>
+	
+	<dl class="ctrlUnit submitUnit">
+		<dt></dt>
+		<dd><input type="submit" value="{xen:phrase reset_password}" accesskey="s" class="button primary" /></dd>
+	</dl>
+	
+	<input type="hidden" name="_xfToken" value="{$visitor.csrf_token_page}" />
+</form>
