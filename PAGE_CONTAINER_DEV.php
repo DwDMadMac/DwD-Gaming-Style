e3b0704c59b70514e58f23ca93197d45f375e5d1
MadMac MacAllister <madmac@PrimLinux.downwithdestruction.net>
2014-04-10 06:39:06 -0400

    Made chatbox into clickavle button as rest.

--- PAGE_CONTAINER_DEV.php
+++ PAGE_CONTAINER_DEV.php
@@ -84,7 +84,6 @@
                         <xen:include template="notices" />
                 </xen:hook>
             </div>
-    <xen:include template="dark_taigachat" />
 <div id="content" class="{$contentTemplate}">
 	<div class="pageWidth">
 		<div class="pageContent">
