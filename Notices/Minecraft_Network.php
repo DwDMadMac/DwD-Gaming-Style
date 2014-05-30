b3f2fb1f16b118e602f6040dd6f0643c16fd3298
MadMac MacAllister <madmac@PrimLinux.downwithdestruction.net>
2014-05-30 06:10:19 -0400

    Added Notices and Pages..

---
+++ Notices/Minecraft_Network.php
@@ -0,0 +1,31 @@
+<div class="patch-notice-outer">
+    <div class="patch-notice-inner">
+        <center>
+            <a href="threads/minecraft-network-pv7-1-0.258/" >
+                <b style="font-size: 25px;color: black;">
+                    DwD Minecraft Network v7.1.0
+                </b> 
+                <xen:comment>
+                    <sup style="vertical-align: top;color: rgb(245, 255, 0);text-shadow: 0px 0px 2px rgb(252, 102, 102);font-size: 18px;">
+                        Beta
+                    </sup>
+                </xen:comment>
+            </a>
+        </center>
+        <br />
+        <div class="message" style="margin: 10px;">
+            <div class="messageText ugc baseHtml">
+            Down With Destruction Gaming (DwD) Strives to make your experience nothing but good times and good vibes. In DwD's Minecraft branch you can experience not just one kind of game type like Role Playing but others too; Creative, Survival (Vanilla Rain), Mini-Games, Deathmatch's, Team Deathmatch's, Maze's, Special Events and much much more! All of DwD's Minecraft game servers are bridge together making joining easy and fast, no need to logout, just teleport over. You can connect to DwD Minecraft Network by using the following IP address:
+            <br />
+            <ul>
+                <li>
+                    <b>hub.dwdg.net</b>
+                </li>
+            </ul>
+            You can connect a few ways DwD's Minecraft Server's when you are logged in. The fastest and easiest way would be with our available commands, which are pretty self explanatory for where they teleport to.
+            <i style="padding-left: 30px;"><pre style="margin: 0 10% !important;text-align: center;"><b>/rpg     /events     /creative     /lobby     /survival     /games</b></pre></i><br />
+            <br />
+            </div>
+        </div>
+    </div>
+</div>
---
+++ Notices/Minecraft_Network_New.php
@@ -0,0 +1,20 @@
+<div class="panel panel-danger panel-150">
+    <div class="panel-heading">
+        <a href="threads/minecraft-network-pv7-1-0.258/" >
+            <b style="font-size: 25px;">
+                DwD Minecraft Network v7.1.0
+            </b>
+        </a>
+    </div>
+    <div class="panel-body">
+        <p>
+            Down With Destruction Gaming (DwD) Strives to make your experience nothing but good times and good vibes. In DwD's Minecraft branch you can experience not just one kind of game type like Role Playing but others too; Creative, Survival (Vanilla Rain), Mini-Games, Deathmatch's, Team Deathmatch's, Maze's, Special Events and much much more! All of DwD's Minecraft game servers are bridge together making joining easy and fast, no need to logout, just teleport over. You can connect to DwD Minecraft Network by using the following IP address:
+            <br />
+            <b style="padding-left: 30px;">hub.dwdg.net</b>
+            <br />
+            You can connect a few ways DwD's Minecraft Server's when you are logged in. The fastest and easiest way would be with our available commands, which are pretty self explanatory for where they teleport to.
+            <i style="padding-left: 30px;"><pre style="margin: 0 10% !important;text-align: center;"><b>/rpg     /events     /creative     /lobby     /survival     /games</b></pre></i><br />
+            <br />
+        </p>
+    </div>
+</div>
